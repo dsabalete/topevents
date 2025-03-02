364e12d81d5a71ea71d4c80b98f9e31723756d0b
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-03-02 16:19:27 +0100

    feat: add sidebar navigation

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="index-page">
+        <aside class="index-page__sidebar">
+            <nav>
+                <ul class="list-none p-0 m-0">
+                    <li class="mb-2">
+                        <nuxt-link to="/recent" class="index-page__sidebar-link" active-class="bg-gray-300">Recent
+                            Events</nuxt-link>
+                    </li>
+                    <li class="mb-2">
+                        <nuxt-link to="/past" class="index-page__sidebar-link" active-class="bg-gray-300">Past
+                            Events</nuxt-link>
+                    </li>
+                    <li class="mb-2">
+                        <nuxt-link to="/contact" class="index-page__sidebar-link""
+                            active-class=" bg-gray-300">Contact</nuxt-link>
+                    </li>
+                    <li class="mb-2">
+                        <nuxt-link to="/cookies" class="index-page__sidebar-link""
+                            active-class=" bg-gray-300">Cookies Policy</nuxt-link>
+                    </li>
+                    <li class="mb-2">
+                        <nuxt-link to="/terms" class="index-page__sidebar-link" active-class="bg-gray-300">Terms &
+                            Conditions</nuxt-link>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+        <main class="flex-grow p-5 box-border">
+            <nuxt />
+        </main>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+/* Remove the custom CSS as it is replaced by Tailwind classes */
+.index-page {
+    @apply flex h-screen;
+
+    &__sidebar {
+        @apply w-48 bg-gray-200 p-5 box-border;
+
+        &__link {
+            @apply block text-gray-800 p-2 rounded hover:bg-gray-300;
+        }
+    }
+}
+</style>
