7429f3b1f837746d08c4d364017b16337db84ffb
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-03-14 13:58:51 +0100

    fix: add logs to the API

--- netlify/functions/api.cjs
+++ netlify/functions/api.cjs
@@ -4,14 +4,22 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const handler = async (event, context) => {
-    const client = new MongoClient(process.env.MONGODB_URI)
-    await client.connect()
-    const db = client.db('tercer_espacio')
-    const data = await db.collection('posters').find({}).toArray()
-    client.close()
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data),
+    try {
+        const client = new MongoClient(process.env.MONGODB_URI)
+        await client.connect()
+        const db = client.db('tercer_espacio')
+        const data = await db.collection('posters').find({}).toArray()
+        client.close()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }
+    } catch (error) {
+        console.error('Database error:', error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Database connection error' })
+        }
     }
 }
 
