9251f73569577980fcc420a7b664bf044529c972
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-03-23 18:46:25 +0100

    feat: add espais feature

---
+++ components/EspaiCard.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const { espai } = defineProps(['espai'])
+</script>
+
+<template>
+    <div class="espai-card">
+        <img :src="espai.picture" :alt="espai.name" class="w-full h-48" />
+        <div class="flex flex-col">
+            <h3 class="text-lg font-bold mb-3">{{ espai.name }}</h3>
+            <p class="text-sm mb-2">{{ espai.address }}</p>
+            <p class="text-sm mb-2">{{ espai.phone }}</p>
+            <p class="text-sm" v-for="horari in espai.timetable" :key="horari">{{ horari }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+.espai-card {
+    @apply grid grid-cols-2 gap-4;
+}
+</style>
--- pages/espais.vue
+++ pages/espais.vue
@@ -1,7 +1,55 @@
+<script lang="ts" setup>
+const espais = [
+    {
+        name: 'FIMA BARCELONA',
+        address: 'Carrer de Melcior de Palau, 140, Sants - Montjuïc, 08014 Barcelona',
+        timetable: [
+            'Obert 8 - 21 (DL - DV)',
+            'Tancat (DS - DG)'
+        ],
+        phone: '93403577',
+        picture: 'https://www.ub.edu/portal/c/document_library/get_file?groupId=5181723&uuid=7c6ddcae-a84b-810b-b44b-9c5d3be94853',
+    },
+    {
+        name: 'ESPAI 2',
+        address: 'Pendent de confirmar',
+        timetable: [
+            'Obert 8 - 21 (DL - DV)',
+            'Tancat (DS - DG)'
+        ],
+        phone: '999999999',
+        picture: 'https://placehold.co/200',
+    },
+    {
+        name: 'ESPAI 3',
+        address: 'Pendent de confirmar',
+        timetable: [
+            'Obert 8 - 21 (DL - DV)',
+            'Tancat (DS - DG)'
+        ],
+        phone: '999999999',
+        picture: 'https://placehold.co/200',
+    },
+    {
+        name: 'ESPAI 4',
+        address: 'Pendent de confirmar',
+        timetable: [
+            'Obert 8 - 21 (DL - DV)',
+            'Tancat (DS - DG)'
+        ],
+        phone: '999999999',
+        picture: 'https://placehold.co/200',
+    }
+]
+</script>
+
 <template>
     <div class="page-espais">
         <h2>Espais</h2>
 
+        <div class="page-espais__container">
+            <EspaiCard v-for="espai in espais" :key="espai.name" :espai="espai" />
+        </div>
     </div>
 </template>
 
@@ -9,8 +57,12 @@
 .page-espais {
     @apply max-w-4xl mx-auto mt-20 p-5;
 
+    &__container {
+        @apply grid grid-cols-1 md:grid-cols-2 gap-5;
+    }
+
     h2 {
-        @apply pb-2;
+        @apply mb-8;
     }
 }
 </style>
