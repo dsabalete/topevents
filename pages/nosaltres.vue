f015c384ccc3c23f9d49a4de1a6ebcdc97e11408
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-05-08 20:19:02 +0200

    feat: update nosaltres

--- pages/nosaltres.vue
+++ pages/nosaltres.vue
@@ -2,8 +2,21 @@
     <div class="page-nosaltres">
         <h2>Nosaltres</h2>
 
-        <p> TBD
+        <p><strong>Tercer Espai</strong> es una iniciativa amb l’objectiu de crear un club de cinema obert,
+            participatiu i
+            comunitari.</p>
+
+        <p>Creiem que el cinema és una bona eina per provocar pensament, generar converses i
+            construir vincles. Per això, organitzem, entre altres esdeveniments, projeccions regulars
+            d’obres audiovisuals —tant de creadores reconegudes com de talents emergents dins la
+            facultat— acompanyades d’espais per debatre, reflexionar i compartir.</p>
+
+        <p>Volem que la FIMA sigui més que un lloc d’estudi: que sigui també un <strong>punt de trobada</strong>, un
+            espai viu on l’amor pel cinema ens connecti i ens inspiri.
         </p>
+
+        <p>Aquest projecte no és només una sala de projecció, és un lloc per mirar, parlar i viure el
+            cinema en comunitat.</p>
     </div>
 </template>
 
@@ -13,6 +26,10 @@
 
     h2 {
         @apply font-bold text-center mb-8;
+    }
+
+    p {
+        @apply mb-6;
     }
 }
 </style>
