f5b5a60e41d0e433ff6ea42434ab7b252ea601c7
David Sabalete <dsabalete@gmail.com>
2025-06-04 22:19:43 +0200

    feat: update background video

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
         <div class="relative w-full h-screen overflow-hidden">
             <video autoplay muted loop class="absolute top-0 left-0 w-full h-full object-cover">
-                <source src="assets/video/mockup.mp4" type="video/mp4">
+                <source src="assets/video/caballo_blanco.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
         </div>
